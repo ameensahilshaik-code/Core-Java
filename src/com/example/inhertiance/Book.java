3abc2fd584e857031f1016ea97138243124bee8c
shaikameensahil <ameensahilshaik@gmail.com>
2025-11-03 10:09:42 +0530

    INHERTIANCE

---
+++ src/com/example/inhertiance/Book.java
@@ -0,0 +1,11 @@
+package com.example.inhertiance;
+
+public class Book {
+    String title;
+    String author;
+
+    void displayInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+    }
+}
---
+++ src/com/example/inhertiance/Novel.java
@@ -0,0 +1,19 @@
+package com.example.inhertiance;
+
+public class Novel extends Book {
+    int pages;
+
+    void displayNovel() {
+        displayInfo();
+        System.out.println("Pages: " + pages);
+    }
+
+    public static void main(String[] args) {
+        Novel n = new Novel();
+        n.title = "Sahil";
+        n.author = "Allah";
+        n.pages = 500000;
+        n.displayNovel();
+    }
+}
+
